9e701d0e104d4783ad4f05033e5adca56bfed3b9
dommalapatidba <dommalapatidba@gmail.com>
2024-12-05 22:56:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "example" {
   ami           = "ami-0aebec83a182ea7ea" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   user_data = file("test.sh")
-  key_name      = aws_key_pair.my_key.key_name
-  security_groups = [aws_security_group.allow_ssh.name]
+  key_name      = my-keypair
+  #security_groups = [aws_security_group.allow_ssh.name]
   
 
   tags = {
@@ -15,45 +15,45 @@ resource "aws_instance" "example" {
   }
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name_prefix = "allow_ssh"
+#resource "aws_security_group" "allow_ssh" {
+  #name_prefix = "allow_ssh"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  #ingress {
+    #from_port   = 22
+    #to_port     = 22
+    #protocol    = "tcp"
+    #cidr_blocks = ["0.0.0.0/0"]
+  #}
+  #ingress {
+   # from_port   = 80
+    #to_port     = 80
+    #protocol    = "tcp"
+    #cidr_blocks = ["0.0.0.0/0"]
+  #}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+  #egress {
+    #from_port   = 0
+    #to_port     = 0
+    #protocol    = "-1"
+    #cidr_blocks = ["0.0.0.0/0"]
+  #}
+#}
 
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-key"
-  public_key = file("~/.ssh/id_ed25519.pub") # Path to your public key
-}
+#resource "aws_key_pair" "my_key" {
+  #key_name   = "my-key"
+  #public_key = file("~/.ssh/id_ed25519.pub") # Path to your public key
+#}
 
-resource "null_resource" "install_apache" {
-  depends_on = [aws_instance.example]
+#resource "null_resource" "install_apache" {
+  #depends_on = [aws_instance.example]
 
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("~/.ssh/id_ed25519") # Path to your private key
-      host        = aws_instance.example.public_ip
-    }
+  #provisioner "remote-exec" {
+    #connection {
+      #type        = "ssh"
+      #user        = "ec2-user"
+      #private_key = file("~/.ssh/id_ed25519") # Path to your private key
+      #host        = aws_instance.example.public_ip
+    #}
 
     inline = [
       "sudo yum update -y",
